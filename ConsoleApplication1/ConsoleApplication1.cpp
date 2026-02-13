591794a0da68d7d267f10adf7d7e0234e6957ba0
Denis <denis.society@yandex.ru>
2026-02-13 10:32:21 +0500

    TestProject

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+int maxdigital(int x){// рекурсия на спуске
+    int s = 3;
+    if (x == 0) {
+        return 0;
+    } else {
+        if (s < (x % 10)) {
+            s = x % 10;
+            maxdigital(x / 10);
+        } else {
+            return s;
+        }
+    }
+}
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int number = 0;
+    int summ = 0;
+    std::cout << "Введите натуральное число: " << std::endl;
+    std::cin >> number;
+    //проверка числа
+    //проверить натуральность
+    if (number < 0) {
+        std::cerr << "Ошибка x < 0";
+        return 1;
+    }
+    summ = maxdigital(number);
+    std::cout << "Сумма цифр числа: " << summ << std::endl;;
+    return 0;
+}
